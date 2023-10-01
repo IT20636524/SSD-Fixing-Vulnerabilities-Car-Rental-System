c1cf4c68ebb65551de2a8c54b44b7228700009f4
IT20636524 <IT20636524@my.sliit.lk>
2023-10-02 00:51:13 +0530

    This framework implicitly discloses version information by default. Make sure it is safe here.,Make sure not using resource integrity feature is safe here. issues fixed

--- car-rental-system/api/index.js
+++ car-rental-system/api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.disable("x-powered-by");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const UserAuthRoute = require("./routes/UserAuth");
