6818821a09242fc38f42925576148358022f4f02
IT20636524 <IT20636524@my.sliit.lk>
2023-10-02 00:41:39 +0530

    Make sure that enabling CORS is safe here.-Fixed Security hotspot

--- car-rental-system/api/index.js
+++ car-rental-system/api/index.js
@@ -17,14 +17,14 @@ const jwt = require("jsonwebtoken");
 
 const cors=require("cors");
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+const corsOptions = {
+  origin: ["http://example.com", "http://localhost:3000"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions)) 
-app.use(cors()) 
+// app.use(cors()) 
 
 dotenv.config();
 app.use(express.json());
